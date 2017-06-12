80f8a48084bed749ad988ea418b232ba90ba21fd
selwynshen <selwynshen@gmail.com>
2017-06-12 15:27:39 +0800

    兼容war部署

--- src/main/kotlin/code/selwyn/Application.kt
+++ src/main/kotlin/code/selwyn/Application.kt
@@ -3,10 +3,11 @@ package code.selwyn
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.ServletComponentScan
+import org.springframework.boot.web.support.SpringBootServletInitializer
 
 @SpringBootApplication
 @ServletComponentScan
-open class Application
+open class Application constructor() : SpringBootServletInitializer()
 
 
 fun main(args: Array<String>) {
